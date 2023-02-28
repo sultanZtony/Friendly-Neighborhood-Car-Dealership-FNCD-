04a52ce2427153fc81bd1abdfef2a00010a67108
JonathanTaing <Jonathan.Taing@colorado.edu>
2023-02-27 22:52:02 -0700

    Replace main.java with Main.java. Filename did not match class name and wouldn't run.

---
+++ Main.java
@@ -0,0 +1,13 @@
+// OOAD Spring 2023 Project 2
+// Friendly Neighborhood Car Dealership (FNCD)
+// Bruce Montgomery - 2/19/23
+// I spent about six hours on this monster...
+// Rename main.java to Main.java. Filename did not match class name and wouldn't run.
+
+public class Main {
+    public static void main(String[] args) {
+        // bootstrap code only - no logic
+        Simulator sim = new Simulator();
+        sim.run();  // Let's do this thing
+    }
+}
